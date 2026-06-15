c07891150b60dd305e9f41a5d047f18ed2fe0dcd
safa <safayasmin567@gmail.com>
2026-06-15 19:11:38 +0530

    i compleate navbar structure

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,16 +4,31 @@ import {FiHeart,FiShoppingCart,FiUser} from "react-icons/fi"
 const Navbar = () => {
   return (
     <div>
-        <div className='fixed top-0 left-0  w-full bg-black border-b border-white/10 backdrop-blur-xl  text-[#f5e6d3] py-4 px-8'>
+        <div className='fixed top-0 left-0  w-full bg-black border-b border-white/10 backdrop-blur-xl  text-[#f5e6d3] py-4 px-8 flex item-center justify-between'>
         <h1 className='text-5xl '
          style={{ fontFamily: "Playfair Display, serif" }}
         > 
-
          bloomcraft
         </h1>
-        </div>
 
-      
+        <div className='text-3xl text-[#f5e6d3] tracking-[6px]'
+        style={{ fontFamily: "Playfair Display, serif" }}
+        >
+        chocolate
+         </div>
+            <input  className='bg-[#f5e6d3] text-black rounded-full py-2.5 px-8'
+            type="text" 
+            placeholder='Search Chocolate...'
+            />
+<div className='flex item-center gap-4'>
+     <FiHeart className='text-xl' />
+     <button className='text-black  bg-[#f5e6d3] flex py-2 '>
+        Add to Cart
+        <FiShoppingCart />
+     </button>
+     <FiUser className='text-xl'/>
+</div>
+        </div> 
     </div>
   )
 }

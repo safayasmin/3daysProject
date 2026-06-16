9e298dc91b510814fdd3db91a37e55394e4ee6af
safa <safayasmin567@gmail.com>
2026-06-16 13:14:21 +0530

    i set a line

--- src/OurPhilosophy.jsx
+++ src/OurPhilosophy.jsx
@@ -5,13 +5,18 @@ const OurPhilosophy = () => {
     <div className='min-h-screen bg-black px-40 py-60'>
         <h1 className='text-[#d4af37] uppercase tracking-[0.35em] text-sm'>Our Philosophy</h1>
 
-        <h2
-            className="text-white text-5xl md:text-6xl mt-5 leading-tight"
+          <h2
+            className="text-white text-6xl mt-5 tracking-[4px] "
             style={{ fontFamily: "Playfair Display, serif" }}
           >
             Mastered in the <br />
-            
+            <span className="italic text-[#d4af37]">
+              Dark Hours
+            </span>
           </h2>
+
+          <div className='w-24 h-[1.5px] bg-[#d4af37] my-8'/>
+          
         
       
     </div>

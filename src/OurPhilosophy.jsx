d14b70d3ebebcc93e50de30a0576dfd8f536b4f5
safa <safayasmin567@gmail.com>
2026-06-16 12:11:42 +0530

    settele a text

--- src/OurPhilosophy.jsx
+++ src/OurPhilosophy.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const OurPhilosophy = () => {
   return (
-    <div className='min-h-screen bg-black'>
+    <div className='min-h-screen bg-black px-40 py-60'>
         <h1 className='text-[#d4af37] uppercase tracking-[0.35em] text-sm'>Our Philosophy</h1>
+
+        <h2
+            className="text-white text-5xl md:text-6xl mt-5 leading-tight"
+            style={{ fontFamily: "Playfair Display, serif" }}
+          >
+            Mastered in the <br />
+            
+          </h2>
         
       
     </div>
